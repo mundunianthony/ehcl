a5e8cad0b38111ffea3e660f056f8106f9ad7847
munduni anthony <mundunianthony@gmail.com>
2025-06-18 10:00:41 +0300

    add healthcheck url

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.http import JsonResponse
 
+def healthcheck(request):
+    return JsonResponse({"status": "ok"})
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),  # All your core app endpoints will be under /api/
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', healthcheck),
 ]
