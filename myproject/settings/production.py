d288fba320af77055473cdcef5879952b48c9ab5
munduni anthony <mundunianthony@gmail.com>
2025-06-21 10:09:00 +0300

    change db url to postgres

--- myproject/settings/production.py
+++ myproject/settings/production.py
@@ -5,6 +5,7 @@ These settings are specific to the production environment.
 """
 import os
 from .base import *  # noqa
+import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
@@ -35,14 +36,11 @@ if not CORS_ALLOWED_ORIGINS or CORS_ALLOWED_ORIGINS == ['']:
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # Email settings
